1c0a4c17bd2107b1c83da428d52db814f7f22d72
Dan Langan <danlangan@Dans-MBP-2.attlocal.net>
2022-07-12 11:56:29 -0500

    edited the settings, URLs, the views for the supers and the super types, as well as the supers serializer

--- supers/serializers.py
+++ supers/serializers.py
@@ -6,3 +6,4 @@ class SupersSerializer(serializers.ModelSerializer):
         model = Supers
         fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type']
         depth = 1
+    super_type = serializers.IntegerField(write_only=True)
--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SupersSerializer
+from .models import Supers
 
-# Create your views here.
+@api_view(['GET'])
+def get_all_supers(request):
+
+    supers = Supers.objects.all()
+    serializer = SupersSerializer(supers, many=True)
+    return Response(serializer.data)
