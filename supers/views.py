5cb5bae5bd33561baa3fb20f6586a60139400df6
Dan Langan <danlangan@Dans-MBP-2.attlocal.net>
2022-07-15 12:58:33 -0500

    edited the serializers for the super_type_id rather than the super_type at the end of the meta class for the supers serializers. also edited the views file ever so slightly to make sure that the custom response could be put in there and show in postman

--- supers/views.py
+++ supers/views.py
@@ -8,8 +8,8 @@ from .models import Supers
 @api_view(['GET', 'POST'])
 def supers_list(request):
     if request.method == 'GET':
-        # super_types = request.query_params.get('type')
-        # supers = Supers.objects.all()
+        #super_types = request.query_params.get('type')
+        #supers = Supers.objects.all()
         villians = Supers.objects.filter(super_type_id=2)
         heroes = Supers.objects.filter(super_type_id=1)
         hero_serializer = SupersSerializer(heroes, many=True)
@@ -24,9 +24,9 @@ def supers_list(request):
         
             
         # else:
-        #     supers = supers.filter(super_type__type=super_types)
-        # serializer = SupersSerializer(supers, many=True)
-        # return Response(serializer.data)
+        #supers = supers.filter(super_type__type=super_types)
+        #serializer = SupersSerializer(supers, many=True)
+        #return Response(serializer.data)
     elif request.method == 'POST':
         serializer = SupersSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
